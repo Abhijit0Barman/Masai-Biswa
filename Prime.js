4ef06d7146189214869c9a89835983bf483231d7
Abhijit0Barman <113384779+Abhijit0Barman@users.noreply.github.com>
2023-02-07 16:34:57 +0530

    Prime.js

---
+++ Prime.js
@@ -0,0 +1,42 @@
+let limit=7;
+
+let flag=0;
+
+for(let i=2; i<=limit; i++)
+
+{
+
+    for(let j=2; j<=i/2; j++)
+
+    {
+
+        if(i%j==0)
+
+        {
+
+            flag++;
+
+        }       
+
+    }
+
+    if(flag==0)
+
+    {
+
+        console.log(j);
+
+
+    }
+
+    else
+
+    {
+
+       flag=0;
+
+    }
+
+    
+
+}
