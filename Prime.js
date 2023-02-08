6a155decfca17cf1075ac0dba4dcdeabf5898e4c
Biswajit Roy <broy16431@gmail.com>
2023-02-08 17:04:51 +0530

    prime_fix_and_palindrome

--- Prime.js
+++ Prime.js
@@ -3,40 +3,22 @@ let limit=7;
 let flag=0;
 
 for(let i=2; i<=limit; i++)
-
 {
 
     for(let j=2; j<=i/2; j++)
-
     {
-
         if(i%j==0)
-
         {
-
             flag++;
-
         }       
-
     }
 
     if(flag==0)
-
     {
-
-        console.log(j);
-
-
+        console.log(i);
     }
-
     else
-
     {
-
        flag=0;
-
     }
-
-    
-
 }
